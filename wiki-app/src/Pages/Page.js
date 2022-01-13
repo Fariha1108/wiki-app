f715c66f5ab094353c749bf9856236db92a0e7bb
FRickReich <frickreich@gmail.com>
2022-01-13 14:32:02 +0100

    Add Page view

--- wiki-app/src/Pages/Page.js
+++ wiki-app/src/Pages/Page.js
@@ -1,9 +1,76 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router';
+import ReactMarkdown from 'react-markdown';
+import Moment from 'react-moment';
+
+// wir importieren die deutschen sprachdaten von moment.
+import 'moment/locale/de';
 
 const Page = () =>
 {
+    // Wir erstellen zwei state hooks für den eintrag und den ladezyklus der daten.
+    const [ entry, setEntry ] = useState({});
+    const [ loading, setLoading ] = useState(true);
+
+    const navigate = useNavigate();
+
+    const { id } = useParams();
+
+    useEffect(() =>
+    {
+        // Wir holen uns den gesuchten eintrag
+        const currentEntry = JSON.parse(window.localStorage.getItem('entries')).find(entry => entry.id === id);
+
+        // Wir befüllen unseren entry hook mit dem gefundenen eintrag
+        setEntry(currentEntry);
+
+        // Wir sagen der applikation, dass das laden beendet wurde
+        setLoading(false);
+    }, []);
+
+    // Wir erstellen eine funktion, um den eintrag zu editieren
+    const handleEditEntry = () =>
+    {
+        // anhand der ID lassen wir uns zum editor navigieren.
+        navigate(`/entry/${ id }/edit`);
+    }
+
+    // Wir erstellen eine funktion, um den aktuellen eintrag löschen zu können
+    const handleDeleteEntry = () =>
+    {
+        console.log('löschen...');
+    }
+
     return(
         <div className="Page">
-            Page
+            {
+                loading ?
+                <p>Loading...</p>
+                :
+                <div>
+                    <h1>{ entry.title }</h1>
+
+                    <div>
+                        <button onClick={ () => handleEditEntry() }>Editieren</button>
+                        <button onClick={ () => handleDeleteEntry() }>Löschen</button>
+                    </div>
+
+                    <ReactMarkdown>{ entry.content }</ReactMarkdown>
+
+                    <hr />
+
+                                {/* non breaking space: &nbsp; */}
+                    Letzte änderung:&nbsp;
+                    <b>
+                        <Moment fromNow locale="de">
+                            {
+                                console.log(entry.timestamp)
+                                // new Date(entry.timestamp).toLocaleString()
+                            }
+                        </Moment>
+                    </b>
+                </div>
+            }
         </div>
     )
 };
